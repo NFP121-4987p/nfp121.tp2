392b2abb120f6faf294f06e1817d45655de2cfc9
Rayane <82977678+rayanehajjar6@users.noreply.github.com>
2021-06-10 16:33:40 +0300

    Update FahrenheitCelsius.java

--- tp2/question2/FahrenheitCelsius.java
+++ tp2/question2/FahrenheitCelsius.java
@@ -2,35 +2,34 @@ package question2;
 
 
 /**
- * Décrivez votre classe FahrenheitCelsius ici.
+ * DÃ©crivez votre classe FahrenheitCelsius ici.
  * 
  * @author (votre nom) 
- * @version (un numéro de version ou une date)
+ * @version (un numÃ©ro de version ou une date)
  */
 public class FahrenheitCelsius{
 
-     /** le point d'entrée de cette application, 
-      * dont le commentaire est à compléter
+     /** le point d'entrÃ©e de cette application, 
+      * dont le commentaire est Ã  complÃ©ter
       *
       *  @param args ...
       */
      public static void main(String[] args){
-       try{
-           for(String s: args) {
-            int fahrenheit = Integer.parseInt(s);
-            float celsius = fahrenheitEnCelsius(fahrenheit);
-            System.out.println(fahrenheit + "\u00B0F -> " + celsius + "\u00B0C"); 
-           }
-       }catch(NumberFormatException nfe){
-           System.out.println("error : " + nfe.getMessage());  
+       for(String s: args){
+            try{
+               int fahrenheit = Integer.parseInt(s);
+               float celsius = fahrenheitEnCelsius(fahrenheit);
+               System.out.println(fahrenheit + "\u00B0F -> " + celsius + "\u00B0C"); 
+            }catch(NumberFormatException nfe){
+               System.out.println("error : " + nfe.getMessage());  
        }
        
      }
      
      /** 
-      * la méthode à compléter. 
-      *   @param f la valeur en degré Fahrenheit
-      *   @return  la conversion en degré Celsius
+      * la mÃ©thode Ã  complÃ©ter. 
+      *   @param f la valeur en degrÃ© Fahrenheit
+      *   @return  la conversion en degrÃ© Celsius
       */
      public static float fahrenheitEnCelsius( int f){
        float c = ((float)5/9) * (f-32);
